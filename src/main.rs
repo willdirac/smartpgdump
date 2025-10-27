34220dda32df7e3e31a0265fe4485e53c814a4ed
willdirac <will@dirac.software>
2025-10-27 17:13:56 +0000

    use clap

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 mod structs;
-use std::env::args;
-use std::path::Path;
+use clap::Parser;
+use std::path::PathBuf;
 use std::{error::Error, process::Command};
 use structs::Schema;
+
+#[derive(Parser)]
+#[command(about = "PostgreSQL schema dump and organize", long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    db_url: String,
+
+    #[arg(short, long)]
+    output_fp: PathBuf,
+}
 
 fn get_dump(db_url: &str) -> Result<String, Box<dyn Error>> {
     let output = Command::new("pg_dump").arg(db_url).arg("-s").output()?;
@@ -16,13 +26,10 @@ fn get_dump(db_url: &str) -> Result<String, Box<dyn Error>> {
     Ok(String::from_utf8(output.stdout)?)
 }
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = args().collect();
+    let args = Args::parse();
 
-    let db_url = &args[1];
-    let base_dir = Path::new(&args[2]);
-    println!("{db_url}");
-    let output = get_dump(db_url)?;
+    let output = get_dump(&args.db_url)?;
     let schema = output.parse::<Schema>()?;
-    schema.write_to_fs(base_dir)?;
+    schema.write_to_fs(&args.output_fp)?;
     Ok(())
 }
